589b1b0a196b5792a07b38093c4b8512316b923b
Erin Ewart <ems1980@hotmail.com>
2022-06-09 23:09:01 -0400

    added index.js files for the routes and finished server.js file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { Note } = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Notedb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set('debug', true);
