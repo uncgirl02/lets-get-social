13ed8194634f2f07f5b64f2053c7f8ee15e5d747
Erin Ewart <ems1980@hotmail.com>
2022-06-09 22:48:52 -0400

    Added Thought and User Model

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Require User and Thought Models
+const thoughts = require('./Thought');
+const users = require('./User');
+
+// Export Modules: User and Thought
+module.exports = { User, Thought };
